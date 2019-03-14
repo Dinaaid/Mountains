200b384a7a92a8a062c06d363cda1bab01970dfa
Dinaaid <dinaaid3@gmail.com>
2019-03-14 16:52:40 +0200

    version1.0

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Scrollspy 
+$(document).ready(function(){
+    // Initializing scrollspy
+    // Initializing scroll snap
+    $(function() {
+          $.scrollify({
+                  section : ".landing-page-section",
+          sectionName : "landing-page-section-scroll",
+          setHeights: false,
+          scrollSpeed: 600,
+          touchScroll: true,
+          interstitialSection: '.footer-main',
+          });
+      });
+    
+    $(".scrollify-action").on('click', function(){
+      if ( $(this).data('scroll') == 'next' ) {
+        $.scrollify.next();
+      }
+      else if ( $(this).data('scroll') == 'prev' ) {
+        $.scrollify.previous();
+      }
+    });
+    
+    // Scrollspy on the floating nav
+    $('body').scrollspy({ target: '.floating-scrollspy' });
+    
+    
+    // Smooth scroll on hash
+    $('.floating-scrollspy  a').on('click',  function(event){
+      event.preventDefault();
+  
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+    });
+    
+  });
+
+  // Navbar
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
